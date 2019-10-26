d327f09d6217e64c5b6a241fbee9a25ec26ec9bc
vagobenedek <37839178+vagobenedek@users.noreply.github.com>
2019-10-26 16:27:36 +0200

    fixed bugs with nucleus motions

--- Assets/Scripts/Behaviours/BrownMotion.cs
+++ Assets/Scripts/Behaviours/BrownMotion.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 public class BrownMotion : MonoBehaviour {
 
   public float radius;
-  public Vector3 position;
   public float scale;
   public float NucleusRadius;
+  float time;
 
 	void Awake() {
-    position = this.transform.position;
     this.transform.localScale = new Vector3(scale, scale, 0);
+    NucleusRadius = this.transform.localScale.x/2;
+    this.transform.Translate(new Vector3(-NucleusRadius/1.65f, 0f,0f));
+    time = Random.Range(0f, 2*Mathf.PI);
   }
-  void FixedUpdate() {
+  void Update() {
+    time += Time.deltaTime;
     float movementAngle = Random.Range(0f, (2*Mathf.PI));
-    Vector3 vectorToBeAdded = new Vector3(0.025f*Mathf.Cos(movementAngle), 0.025f*Mathf.Sin(movementAngle), 0f);
-    this.transform.Translate(vectorToBeAdded);
-    if (Mathf.Sqrt(this.transform.localPosition.x*this.transform.localPosition.x + this.transform.localPosition.y*this.transform.localPosition.y)>=(radius-NucleusRadius)*(radius-NucleusRadius)) {
+    Vector3 vectorToBeAdded = new Vector3(radius*Mathf.Sin(Time.time/2)/4, radius*Mathf.Cos(2*time/2)/4, 0f);
+    this.transform.localPosition = (vectorToBeAdded);
+    /*if (Mathf.Sqrt(this.transform.localPosition.x*this.transform.localPosition.x + this.transform.localPosition.y*this.transform.localPosition.y)>=(radius-NucleusRadius)) {
       this.transform.Translate(-vectorToBeAdded);
-    }
+    }*/
   }
 }
